fe2bab25186ebb18fcd1a68432a4998c14e1e38b
anand mukut <anand@anands-macbook-pro.local>
2018-01-12 11:17:10 +0530

    Initial Commit

---
+++ web/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  web
+//
+//  Created by anand mukut tirkey on 12/01/18.
+//  Copyright © 2018 anand mukut tirkey. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ web/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  web
+//
+//  Created by anand mukut tirkey on 12/01/18.
+//  Copyright © 2018 anand mukut tirkey. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
